8a1b2b2ecb9292ebae0d1d2340bf1b7c6ef61356
Mark Przepiora <mark.przepiora@gmail.com>
2014-06-13 23:02:54 -0600

    Add socket.io to index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// var app              = require('express')(),
-//     // io               = require('socket.io').listen(app),
-//     coffeeMiddleware = require('coffee-middleware')({ src: __dirname + '/views' }),
-//     serveStatic      = require('serve-static')(__dirname + 'views');
-
 var express = require('express'),
     app     = express(),
     path    = require('path'),
     coffee  = require('coffee-middleware'),
-    static  = require('serve-static');
+    static  = require('serve-static'),
+    server  = require('http').Server(app),
+    io      = require('socket.io')(server);
 
 app.use(static(__dirname + '/views'));
 app.use(coffee({ src: __dirname + '/views' }));
+
 app.listen(8080);
 
-// // routing
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
+app.get('/foo', function (req, res) {
+  res.send('hello world');
+});
