31ac76b7be82bdd43f4d7c596e3b8cc4260f4114
Mark Przepiora <mark.przepiora@gmail.com>
2014-06-14 07:56:05 -0600

    Add baconjs and node-uuid

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var express = require('express'),
     coffee  = require('coffee-middleware'),
     static  = require('serve-static'),
     server  = require('http').Server(app),
-    io      = require('socket.io')(server);
+    io      = require('socket.io')(server),
+    bacon   = require('baconjs').Bacon,
+    uuid    = require('node-uuid');
 
 app.use(static(__dirname + '/views'));
 app.use(coffee({ src: __dirname + '/views' }));
@@ -12,5 +14,6 @@ app.use(coffee({ src: __dirname + '/views' }));
 server.listen(8080);
 
 io.on('connection', function (socket) {
+  console.log(socket.constructor);
   io.emit('welcome', 'Hi there!!!');
 });
